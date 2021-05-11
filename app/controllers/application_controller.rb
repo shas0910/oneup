1757e74ceaa5fbbf5c315d15cc3cf63becda48b2
sproutVEVO <kageusu@gmail.com>
2021-05-11 17:24:18 +0900

    新規登録、ログイン、ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :user_desc, :user_image, :user_cover_image])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_one_attached :user_image
+  has_one_attached :user_cover_image
+
+  validates :user_name, presence: true
+end
